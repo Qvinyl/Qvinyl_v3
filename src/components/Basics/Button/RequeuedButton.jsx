dcfef6cdfe57a474f114a281112ab1053e1e2cf8
Josh Cheung <jtcheung97@gmail.com>
2023-02-06 16:42:20 -0800

    clean up

--- src/components/Basics/Button/RequeuedButton.jsx
+++ src/components/Basics/Button/RequeuedButton.jsx
@@ -33,11 +33,8 @@ const RequeuedButton = styled(Button)({
   },
   '&:active': {
     boxShadow: 'none',
-    // backgroundColor: '#0062cc',
-    // borderColor: '#005cbf',
   },
   '&:focus': {
-    // boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
   },
 });
 
