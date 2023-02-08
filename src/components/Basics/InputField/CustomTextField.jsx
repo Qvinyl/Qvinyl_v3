ff35d2caa7346502b2587f64327bf7ed3e74066f
Josh Cheung <jtcheung97@gmail.com>
2023-02-07 16:01:43 -0800

    changing color of text

--- src/components/Basics/InputField/CustomTextField.jsx
+++ src/components/Basics/InputField/CustomTextField.jsx
@@ -4,9 +4,13 @@ import { styled } from '@mui/material/styles';
 const CustomTextField = styled(TextField)({
     '& .MuiInput-underline:before': {
         borderBottomColor: '#ACACAC',
+        color: "#ACACAC"
     },
     '& .MuiInputLabel-root': {
         color: "#ACACAC",
+    },
+    '& .MuiInput-input': {
+        color: "#ACACAC"
     }
 })
 export default CustomTextField;
