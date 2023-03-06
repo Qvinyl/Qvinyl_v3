aa3a26f80568d76a4e1d9937e68338cf0a93282d
Josh Cheung <jtcheung97@gmail.com>
2023-03-06 12:52:47 -0800

    small tweak

--- src/features/userService/UserAuthentication.js
+++ src/features/userService/UserAuthentication.js
@@ -2,7 +2,6 @@ import { signOut } from "firebase/auth";
 const auth = require('../../config/constraints').firebaseAuth;
 
 export async function logout() {
-    console.log(auth);
     signOut(auth).then((results) => {
         return true;
     }).catch((error) => {
