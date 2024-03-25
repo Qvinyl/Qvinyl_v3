bacbb4b7ee75428343f6d843ae04bbfb41f33bdd
Josh Cheung <JTCheung97@gmail.com>
2024-03-25 11:37:09 -0700

    small tweak

--- src/config/db_config.js
+++ src/config/db_config.js
@@ -1,6 +1,7 @@
-// const HOSTSITE =  "184.169.149.49";
+// const HOSTSITE =  "localhost";
 const HOSTSITE = "vbox.api.qvinyl.app";
-const CON_PATH = "/conference/";
+// const CON_PATH = "/conference/";
+const CON_PATH = "/callMeAPI";
 
 
 module.exports = {
@@ -12,25 +13,26 @@ module.exports = {
     FB_MESSAGING_SENDER_ID: "1095197449779",
     FB_APP_ID: "1:1095197449779:web:7efbd809f751e285800d46",
 
-    CONNECTION_TYPE: "https://",
+    // CONNECTION_TYPE: "https://",
+    CONNECTION_TYPE: "http://",
     // HOSTSITE: "localhost",
     // HOSTSITE_2: "34.82.95.65",
     // HOSTSITE: "api.staging1.qvinyl.app",
     // HOSTSITE: "vbox.api.qvinyl.app",
     HOSTSITE: `${HOSTSITE}`,
-    SYNC_PATH: "/syncing/",
-    YTS_PATH: "/youtubeSearch/",
-    MSG_PATH: "/messaging/",
-    NOT_PATH: "/notifications/",
-    CON_PATH: CON_PATH,
-    ORM_PATH: "/data-sync/",
+    // SYNC_PATH: "/syncing/",
+    // YTS_PATH: "/youtubeSearch/",
+    // MSG_PATH: "/messaging/",
+    // NOT_PATH: "/notifications/",
+    // CON_PATH: CON_PATH,
+    // ORM_PATH: "/data-sync/",
 
 
     // SYNC_PATH: ":7777",
     // YTS_PATH: ":9000/search",
     // MSG_PATH: ":9993",
     // NOT_PATH: ":5240",
-    // ORM_PATH: ":3000/api/v1/",
+    ORM_PATH: ":3000/api/v1/",
 
     // PEER_CONFIG: {
     //     host: `${HOSTSITE}`,
@@ -41,6 +43,7 @@ module.exports = {
     PEER_CONFIG: {
         host: `${HOSTSITE}`,
         path: `${CON_PATH}`,
+        secure: true
     }
 };
 
